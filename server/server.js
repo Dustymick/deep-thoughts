aa3e6bf535d287ee7645e9f63262b5de96648e91
Dustin Micklos <damicklo5@gmail.com>
2021-05-06 16:55:19 -0500

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-const { authMiddleware } = require('./utils/auth');
 
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
